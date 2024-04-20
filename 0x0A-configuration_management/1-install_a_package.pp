850e8374b7cbfd3d5bc99b98d927e7d5352fb585
Nzube root <momafransisco@gmail.com>
2024-04-20 02:05:16 +0000

    mason

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Exec['install_pip'], # You might need to ensure pip is installed first
+}
+
+exec { 'install_pip':
+  command => 'apt-get install -y python3-pip', # This command might vary depending on your system
+  unless  => 'which pip3',
+  path    => ['/bin', '/usr/bin'],
 }
