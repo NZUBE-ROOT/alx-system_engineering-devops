3adb95c3c10b9ef8ab297e71c9118a498d7de39e
Nzube root <momafransisco@gmail.com>
2024-04-20 01:49:04 +0000

    jokeaside

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Install a package from pip3 - flask version 2.1.0
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-venv':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
